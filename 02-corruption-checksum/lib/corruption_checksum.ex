b442b58aa632a46e374b52f28ddbad70a6c1bb73
Richard Livsey <richard@livsey.org>
2017-12-09 22:17:16 +0000

    mix new day 2

---
+++ 02-corruption-checksum/lib/corruption_checksum.ex
@@ -0,0 +1,18 @@
+defmodule CorruptionChecksum do
+  @moduledoc """
+  Documentation for CorruptionChecksum.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CorruptionChecksum.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 02-corruption-checksum/test/corruption_checksum_test.exs
@@ -0,0 +1,8 @@
+defmodule CorruptionChecksumTest do
+  use ExUnit.Case
+  doctest CorruptionChecksum
+
+  test "greets the world" do
+    assert CorruptionChecksum.hello() == :world
+  end
+end
