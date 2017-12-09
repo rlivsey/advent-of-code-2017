f36941a6b3341ff887969b2899d5a7dcde484a0a
Richard Livsey <richard@livsey.org>
2017-12-09 22:12:26 +0000

    mix new

---
+++ 01-inverse-captcha/lib/inverse_captcha.ex
@@ -0,0 +1,18 @@
+defmodule InverseCaptcha do
+  @moduledoc """
+  Documentation for InverseCaptcha.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InverseCaptcha.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 01-inverse-captcha/test/inverse_captcha_test.exs
@@ -0,0 +1,8 @@
+defmodule InverseCaptchaTest do
+  use ExUnit.Case
+  doctest InverseCaptcha
+
+  test "greets the world" do
+    assert InverseCaptcha.hello() == :world
+  end
+end
