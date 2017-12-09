4797a4d947611a8f4c5ba50da17476bad560d822
Richard Livsey <richard@livsey.org>
2017-12-09 22:44:18 +0000

    Solve day 2 - corruption checksum

--- 02-corruption-checksum/lib/corruption_checksum.ex
+++ 02-corruption-checksum/lib/corruption_checksum.ex
@@ -1,18 +1,46 @@
 defmodule CorruptionChecksum do
   @moduledoc """
-  Documentation for CorruptionChecksum.
+  CorruptionChecksum.
   """
 
   @doc """
-  Hello world.
+  The spreadsheet consists of rows of apparently-random numbers.
+  To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum.
+  For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
 
-  ## Examples
+  For example, given the following spreadsheet:
 
-      iex> CorruptionChecksum.hello
-      :world
+  5 1 9 5
+  7 5 3
+  2 4 6 8
 
+  The first row's largest and smallest values are 9 and 1, and their difference is 8.
+  The second row's largest and smallest values are 7 and 3, and their difference is 4.
+  The third row's difference is 6.
+
+  In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
   """
-  def hello do
-    :world
+  def solve(str) when is_binary(str) do
+    str
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(fn(line) ->
+      line
+      |> String.split
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> solve
+  end
+
+  def solve(grid) when is_list(grid) do
+    grid
+    |> Enum.map(&line_checksum/1)
+    |> Enum.sum
+  end
+
+  defp line_checksum(line) do
+    min = Enum.min(line)
+    max = Enum.max(line)
+    max - min
   end
 end
--- 02-corruption-checksum/test/corruption_checksum_test.exs
+++ 02-corruption-checksum/test/corruption_checksum_test.exs
@@ -3,6 +3,10 @@ defmodule CorruptionChecksumTest do
   doctest CorruptionChecksum
 
   test "greets the world" do
-    assert CorruptionChecksum.hello() == :world
+    assert """
+    5 1 9 5
+    7 5 3
+    2 4 6 8
+    """ |> CorruptionChecksum.solve == 18
   end
 end
